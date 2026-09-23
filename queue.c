aa5add0cdc7ba5761301dd35368c8c116844b981
eenguun <eenguun7003@gmail.com>
2026-09-24 00:26:00 +0800

    enguun
    Implemented push and pop operations for the queue.

--- queue.c
+++ queue.c
@@ -3,13 +3,22 @@
 /* p-ийн зааж буй Queue-д x утгыг хийнэ */
 void q_push(Queue *p, int x)
 {
-        /* Энд оруулах үйлдлийг хийнэ үү */
+        p->q_arr[p->q_len] = x;
+        p->q_len++;
 }
 
 /* p-ийн зааж буй Queue-с гаргана */
 void q_pop(Queue *p)
 {
-        /* Энд гаргах үйлдлийг хийнэ үү */
+        int i;
+
+        if (p->q_len > 0) {
+                for (i = 0; i < p->q_len - 1; i++) {
+                        p->q_arr[i] = p->q_arr[i + 1];
+                }
+
+                p->q_len--;
+        }
 }
 
 /* p-ийн зааж буй Queue-н утгуудыг хэвлэнэ */
